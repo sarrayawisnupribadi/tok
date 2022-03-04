c0fe3be489af5ddf6b482c6aac5f0dc6798d885d
sarrayawisnupribadi <87954817+sarrayawisnupribadi@users.noreply.github.com>
2022-03-04 20:49:53 +0700

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Header'
-import card2 from './assets/card2-1.png'
+import card from './assets/card4.png'
 import './styles/Section.css'
 
 const Section = () => {
@@ -10,15 +10,14 @@ const Section = () => {
 
             <div className="section__box">
                 <div className="section__content">
-                    <h1 className='content__header'>Pakingg</h1>
-                    <p>OPIOPIOPI</p>
-                    <p className='small__p'>The Card of Future</p>
+                    <h1 className='content__header'>C-Ad</h1>
+                    <p className='small__p'>For your better business</p>
                     <button>Get</button>
                     <button>Discover</button>
                 </div>
 
                 <div className="section__img">
-                    <img src={card2} alt="" />
+                    <img src={card} alt="" />
                 </div>
 
 
